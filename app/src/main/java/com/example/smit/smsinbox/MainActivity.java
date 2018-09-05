26eb7221180a8d2fe7b07e9942919b294349664e
pritsorathiya5181 <prit.sorathiya6939@gmail,com>
2018-09-05 18:27:24 +0530

    Just comment 4

--- app/src/main/java/com/example/smit/smsinbox/MainActivity.java
+++ app/src/main/java/com/example/smit/smsinbox/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //sbkabjabfa
+        //FBAABJFA
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
